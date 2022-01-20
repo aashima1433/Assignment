e052a6761ca58987fd26c9e757c7bd9229f17222
aashima <kansalashima9@gmail.com>
2022-01-20 23:21:18 +0530

    update que route

--- Routes/index.js
+++ Routes/index.js
@@ -3,12 +3,15 @@ const router = express.Router();
 import registerController from '../controllers/registerController';
 import loginController from '../controllers/loginController';
 import queController from '../controllers/queController';
-import voteController from '../controllers/voteController'
-import ansController from '../controllers/ansController'
+import voteController from '../controllers/voteController';
+import ansController from '../controllers/ansController';
 
 router.post('/register',registerController.register)
 router.post('/login',loginController.login)
 router.post('/addquestion/:id',queController.postque)
 router.post('/addanswer/:id/:userid',ansController.postans)
 router.put('/vote', voteController.vote)
+router.put('/updatequestion/:id',queController.updateque)
+
+
 export default router
