2c20ceb0ef901cb772f44ca65acd3b0048baf3a9
aashima <kansalashima9@gmail.com>
2022-01-20 23:02:40 +0530

    updated

--- Routes/index.js
+++ Routes/index.js
@@ -2,12 +2,12 @@ import express from 'express'
 const router = express.Router();
 import registerController from '../controllers/registerController';
 import loginController from '../controllers/loginController';
-
+import queController from '../controllers/queController';
 
 
 router.post('/register',registerController.register)
 router.post('/login',loginController.login)
-router.post('/addquestion/:id',auth,queController.postque)
+router.post('/addquestion/:id',queController.postque)
 
 
 export default router
