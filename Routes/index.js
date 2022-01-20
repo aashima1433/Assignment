e7dc39b81b8b774fec27df1db9e0c1f2c446069d
aashima <kansalashima9@gmail.com>
2022-01-20 22:59:43 +0530

    add question route

--- Routes/index.js
+++ Routes/index.js
@@ -7,7 +7,7 @@ import loginController from '../controllers/loginController';
 
 router.post('/register',registerController.register)
 router.post('/login',loginController.login)
-
+router.post('/addquestion/:id',auth,queController.postque)
 
 
 export default router
