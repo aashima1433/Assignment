ee9c953fee15435565c053b190e30503aed0bdd1
aashima <kansalashima9@gmail.com>
2022-01-20 21:24:10 +0530

    login controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,16 +8,33 @@ import JwtService from '../services/JwtService';
 const loginController = {
     async login(req, res, next) {
         // Validation
-        const loginSchema = Joi.object({
+        const login = Joi.object({
             email: Joi.string().email().required(),
             password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
         });
-        const { error } = loginSchema.validate(req.body);
+        const { error } = login.validate(req.body);
 
         if (error) {
             return next(error);
         }
+    
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                return next(error+"wrong credentials");
+            }
+            const matchpass = await bcrypt.compare(req.body.password, user.password);
+            if (!matchpass) {
+                return next(error+"wrong credentials");
+            }
+            // Token
+            const token = JwtService.sign({ _id: user._id });
+   
+            res.json(token);
 
+        } catch(err) {
+            return next(err);
+        }
 
     },
 
